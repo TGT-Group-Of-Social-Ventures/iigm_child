d2dba26802b63cd1c54a4300745dba2f925364cc
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-12-09 13:27:45 +0530

    added things

--- src/pages/companies.js
+++ src/pages/companies.js
@@ -22,7 +22,7 @@ const companies = [
     createdAt: '27/03/2019',
     description: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
     logo: '/assets/logos/logo-dropbox.png',
-    title: 'Dropbox',
+    title: 'CGF',
     downloads: '594'
   },
   // {
