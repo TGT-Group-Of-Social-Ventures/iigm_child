f1943833ec679ff308fc29ac4e5f0d114cded60e
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2023-12-26 15:38:01 +0530

    added dashboard instead of overview

--- src/layouts/dashboard/config.js
+++ src/layouts/dashboard/config.js
@@ -10,7 +10,7 @@ import { SvgIcon } from '@mui/material';
 
 export const items = [
   {
-    title: 'Overview',
+    title: 'Dashboard',
     path: '/',
     icon: (
       <SvgIcon fontSize="small">
