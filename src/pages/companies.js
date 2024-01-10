df101535b2187eb482c3bb3384fa3cb6156ea928
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2024-01-10 16:54:51 +0530

    added content

--- src/pages/companies.js
+++ src/pages/companies.js
@@ -20,27 +20,27 @@ const companies = [
   {
     id: '2569ce0d517a7f06d3ea1f24',
     createdAt: '27/03/2019',
-    description: 'Dropbox is a file hosting service that offers cloud storage, file synchronization, a personal cloud.',
+    description: 'This is the Flagship course of IIGMA. Be employment ready with a 6 months rigorous module on all facets of Livestock Entrepreneurship and Business Management.',
     logo: '/assets/logos/logo-dropbox.png',
     title: 'CGF',
-    downloads: '594'
+    downloads: '15 Students Enrolled'
   },
-  // {
-  //   id: 'ed2b900870ceba72d203ec15',
-  //   createdAt: '31/03/2019',
-  //   description: 'Medium is an online publishing platform developed by Evan Williams, and launched in August 2012.',
-  //   logo: '/assets/logos/logo-medium.png',
-  //   title: 'Medium Corporation',
-  //   downloads: '625'
-  // },
-  // {
-  //   id: 'a033e38768c82fca90df3db7',
-  //   createdAt: '03/04/2019',
-  //   description: 'Slack is a cloud-based set of team collaboration tools and services, founded by Stewart Butterfield.',
-  //   logo: '/assets/logos/logo-slack.png',
-  //   title: 'Slack',
-  //   downloads: '857'
-  // },
+  {
+    id: 'ed2b900870ceba72d203ec15',
+    createdAt: '31/03/2019',
+    description: 'Medium is an online publishing platform developed by Evan Williams, and launched in August 2012.',
+    logo: '/assets/logos/logo-medium.png',
+    title: 'Livestock based livelihood project management',
+    downloads: 'Coming Soon'
+  },
+  {
+    id: 'a033e38768c82fca90df3db7',
+    createdAt: '03/04/2019',
+    description: 'Slack is a cloud-based set of team collaboration tools and services, founded by Stewart Butterfield.',
+    logo: '/assets/logos/logo-slack.png',
+    title: 'Livestock business counselors self certification course',
+    downloads: 'Coming soon'
+  }
   // {
   //   id: '1efecb2bf6a51def9869ab0f',
   //   createdAt: '04/04/2019',
