1076a12d01acaf8e2cc5127f9e92f6ea79e231f1
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2024-03-19 13:09:32 +0530

    added deletion

--- src/pages/auth/deleteAccount.js
+++ src/pages/auth/deleteAccount.js
@@ -2,15 +2,26 @@ import React from "react";
 import { getAuth, deleteUser, signOut } from "firebase/auth";
 import { db, auth } from "src/firebaseConfig";
 import { useRouter } from "next/router";
+import { doc, deleteDoc } from "firebase/firestore";
 
 export default function DeleteAccount() {
   const user = auth.currentUser;
   const router = useRouter();
 
+  const deleteFromDatabase = async (uid) => {
+    try {
+      const response = await deleteDoc(doc(db, "users", uid));
+      console.log("delete", response);
+    } catch (error) {
+      console.log("error deleting from firestore", error);
+    }
+  };
+
   const handleDelete = async () => {
     // await db.collection("users").doc(uid).delete;
     try {
       const uid = user.uid;
+      await deleteFromDatabase(uid);
       const response = await deleteUser(user);
       console.log("res", response);
       alert("Success");
