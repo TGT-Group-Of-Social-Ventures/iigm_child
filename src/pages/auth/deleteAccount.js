70fb6577e71630570e3d736fb95a0a93e013c9e1
Kishan Mishra <98342089+kishankool@users.noreply.github.com>
2024-03-19 12:39:43 +0530

    added deletion

--- src/pages/auth/deleteAccount.js
+++ src/pages/auth/deleteAccount.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { getAuth, deleteUser, signOut } from "firebase/auth";
 import { db, auth } from "src/firebaseConfig";
+import { useRouter } from "next/router";
 
 export default function DeleteAccount() {
   const user = auth.currentUser;
+  const router = useRouter();
 
   const handleDelete = async () => {
     // await db.collection("users").doc(uid).delete;
     try {
       const uid = user.uid;
-      const response = deleteUser(user);
+      const response = await deleteUser(user);
       console.log("res", response);
       alert("Success");
+      router.push("/auth/firebaseAuth");
     } catch (error) {
       alert("error deleting data");
       console.log("error", error);
@@ -24,6 +27,7 @@ export default function DeleteAccount() {
     try {
       const response = await signOut(auth);
       console.log("res", response);
+      router.push("/auth/firebaseAuth");
     } catch (error) {
       alert("error signing out");
       console.log("error", error);
--- src/pages/auth/firebaseAuth.js
+++ src/pages/auth/firebaseAuth.js
@@ -1,14 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { auth, signInWithGooglePopup } from "../../firebaseConfig/index";
-import { RecaptchaVerifier, signInWithPhoneNumber, getAuth } from "firebase/auth";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import { useRouter } from "next/router";
 
 const Login = () => {
-  const auth1 = getAuth();
-  console.log(auth);
+  const router = useRouter();
   const [phone, setPhone] = useState("+91");
   const [otp, setOtp] = useState("");
   const [verificationId, setVerificationId] = useState(null);
   const [error, setError] = useState(null);
+  const [recaptcha, setRecaptcha] = useState(null);
+  const [showCaptcha, setShowCaptcha] = useState(true);
+  const [confirmation, setConfirmation] = useState(null);
+
+  useEffect(() => {
+    let recaptchaVerifier;
+    recaptchaVerifier = new RecaptchaVerifier(auth, "recaptcha", {
+      size: "invisible",
+    });
+    setRecaptcha(recaptchaVerifier);
+  }, []);
 
   const handlePhoneChange = (e) => {
     setPhone(e.target.value);
@@ -18,55 +29,49 @@ const Login = () => {
     setOtp(e.target.value);
   };
 
-  window.recaptchaVerifier = new RecaptchaVerifier(auth1, "sign-in-button", {
-    size: "invisible",
-    callback: (response) => {
-      // reCAPTCHA solved, allow signInWithPhoneNumber.
-      handleSendOtp();
-    },
-  });
-
   const handleSendOtp = async () => {
-    let appVerifier = window.recaptchaVerifier;
-
-    signInWithPhoneNumber(auth1, phone, appVerifier)
-      .then((confirmationResult) => {
-        // SMS sent. Prompt user to type the code from the message, then sign the
-        // user in with confirmationResult.confirm(code).
-        window.confirmationResult = confirmationResult;
-      })
-      .catch((error) => {
-        window.recaptchaVerifier.render().then(function (widgetId) {
-          grecaptcha.reset(widgetId);
-        });
-        console.log(error);
-      });
+    try {
+      const response = await signInWithPhoneNumber(auth, phone, recaptcha);
+      alert("otp sended successfully");
+      setShowCaptcha(false);
+      setConfirmation(response);
+    } catch (error) {
+      switch (error.code) {
+        case "auth/too-many-requests":
+          alert("Too many requests. Please try again later.");
+          break;
+        case "auth/invalid-phone-number":
+          alert("The phone number is invalid.");
+          break;
+        default:
+          alert("Something went wrong. Please try again later.");
+          break;
+      }
+      console.log(error);
+    }
   };
 
-  const handleSignInWithOtp = () => {
-    if (otp.length === 6) {
-      // verifu otp
-      let confirmationResult = window.confirmationResult;
-      confirmationResult
-        .confirm(otp)
-        .then((result) => {
-          // User signed in successfully.
-          let user = result.user;
-          console.log(user);
-          alert("User signed in successfully");
-          // ...
-        })
-        .catch((error) => {
-          // User couldn't sign in (bad verification code?)
-          // ...
-          alert("User couldn't sign in (bad verification code?)");
-        });
+  const handleSignInWithOtp = async () => {
+    try {
+      if (otp.length === 6) {
+        const data = await confirmation.confirm(otp);
+        router.push("/auth/deleteAccount");
+        console.log(data);
+      }
+    } catch (error) {
+      alert("Failed verifying otp");
+      console.log("error verifying otp", error);
     }
   };
 
   const handleSignInWithGoogle = async () => {
-    const response = await signInWithGooglePopup();
-    console.log(response);
+    try {
+      const response = await signInWithGooglePopup();
+      console.log(response);
+      router.push("/auth/deleteAccount");
+    } catch (error) {
+      console.log("error logging in", error);
+    }
   };
 
   return (
@@ -81,6 +86,7 @@ const Login = () => {
       <button onClick={handleSignInWithOtp}>Sign In with OTP</button>
       <button onClick={handleSignInWithGoogle}>Sign In with Google</button>
       {error && <p>{error}</p>}
+      {showCaptcha && <div id="recaptcha"></div>}
     </div>
   );
 };
